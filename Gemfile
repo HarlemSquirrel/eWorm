03087f6b6ec20716a05f675b6890ffd99166bc35
Kevin McCormack <wittyman37@yahoo.com>
2016-02-29 18:42:45 -0500

    fix rspec tests not running

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
 
   # secure user management
   gem 'devise'
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a username" do
-    expect(user).to respond_to(:username)
+  before(:each) do
+    @user = User.create
   end
 
+  describe 'User' do
+    it "has a username" do
+      expect(@user).to respond_to(:username)
+    end
+
+    it "has an email" do
+      expect(@user).to respond_to(:email)
+    end
+  end
 end
