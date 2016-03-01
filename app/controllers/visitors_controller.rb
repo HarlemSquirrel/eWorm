b64812ca81f7971a6c5ed6b689b1c9334ab411c7
Kevin McCormack <wittyman37@yahoo.com>
2016-03-01 18:10:33 -0500

    style home page and main layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,7 @@ class VisitorsController < ApplicationController
     @num_books = Book.all.count
     @num_reviews = Review.all.count
     @num_users = User.all.count
+    @reviews= Review.all[(@num_reviews-4)..(@num_reviews-1)]
   end
 
   def about
