e66a4527a38b175dfe0cae33cee0b7413789cfa0
Kevin McCormack <wittyman37@yahoo.com>
2016-03-01 14:58:54 -0500

    update index to be dynamic for visitors vs users and display basic stats

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,8 @@
 class VisitorsController < ApplicationController
   def index
+    @num_books = Book.all.count
+    @num_reviews = Review.all.count
+    @num_users = User.all.count
   end
 
   def about
